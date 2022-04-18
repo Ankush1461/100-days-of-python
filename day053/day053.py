92d0b9694443c3fdb7a96fb8aa702d78e7436e04
Ankush Karmakar <karmakarankush1461@gmail.com>
2022-04-18 23:35:01 +0530

    Add files via upload

---
+++ day053/day053.py
@@ -0,0 +1,55 @@
+# Zillow Rental Program
+
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36 Edg/100.0.1185.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get("https://www.zillow.com/homes/Texas-City,-TX_rb/", headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".list-card-top a")
+
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    print(href)
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".list-card-details li")
+all_prices = [price.get_text().split("+")[0] for price in all_price_elements if "$" in price.text]
+
+chrome_driver_path = Service("C:\development\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=chrome_driver_path)
+
+for n in range(len(all_links)):
+    driver.get("https://forms.gle/jU2Uz4RSrQWn24Zq5")
+
+    time.sleep(2)
+    address = driver.find_element(By.XPATH,
+                                  '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]/textarea')
+    price = driver.find_element(By.XPATH,
+                                '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH,
+                               '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
