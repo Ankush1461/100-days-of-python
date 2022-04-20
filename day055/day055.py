810588bd93b7d1876770cbf55633a0bb4e333740
Ankush Karmakar <karmakarankush1461@gmail.com>
2022-04-20 10:38:03 +0530

    Add files via upload

---
+++ day055/day055.py
@@ -0,0 +1,33 @@
+# Higher Lower Url Program
+
+from flask import Flask
+import random
+
+random_number = random.randint(0, 9)
+print(random_number)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/6jHICQwqR0Oli/giphy.gif'/>"
+
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/ykS9B0nqIydvq/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/EXEEJHIPpT4ZO/giphy.gif'/>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
