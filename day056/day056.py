abc75a972716809e38117f05c8eab039d0cc932e
Ankush Karmakar <karmakarankush1461@gmail.com>
2022-04-22 12:03:01 +0530

    Add files via upload

---
+++ day056/day056.py
@@ -0,0 +1,14 @@
+# Name Card Program (Rendering HTML static files using Flask)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
