9762eb38c700a44a5eb3dc36c88757745c0171b8
Ankush Karmakar <karmakarankush1461@gmail.com>
2022-04-25 21:28:34 +0530

    Add files via upload

---
+++ day060/main.py
@@ -0,0 +1,53 @@
+# Improved Blog Program( Contact Form Submission And Send Mail)
+
+import smtplib
+
+import requests
+from flask import Flask, render_template, request
+
+posts = requests.get("https://api.npoint.io/a957ceed601019c1896d").json()
+OWN_EMAIL = "ankushkarmakarmail@gmail.com"
+OWN_PASSWORD = "ankush12345"
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
